8b99d3e6f980fe37564247d88b55593280283746
HeBianGu <908293466@qq.com>
2017-03-17 09:34:03 +0800

    增加单元测试

---
+++ SimalorManager.Tester/ServiceTester.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OPT.Product.SimalorManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimalorManager.Tester
+{
+    [TestClass]
+    public class ServiceTester
+    {
+
+        /// <summary> 测试加载Eclipse数据 </summary>
+        [TestMethod]
+        public void ThreadLoadResize()
+        {
+            string filePath = @"D:\WorkArea\LaoBB\3106-Eclipse格式\3106_E100.DATA";
+
+            var data = FileFactoryService.Instance.ThreadLoadResize(filePath);
+
+
+            //// Todo ：单元测试用断言检验运行情况 
+            //Assert.IsFalse(true);
+        }
+    }
+}
